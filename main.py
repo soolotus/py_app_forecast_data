81be1ac569772104c37bf5261ee831fd9d0811cd
SooYeon Seong <soolotus632@gmail.com>
2024-12-29 09:28:38 -0700

    adding config files

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
 days = st.slider("Forecast Days", min_value=1, max_value=5, help="Select the number of forecasted days.")
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
-st.subheader(f"{option} for the next {days} days in {place}")
+st.subheader(f"{option} for the next {days} days in {place.title()}")
 if place:
     try:
         filtered_data = get_data(place, days)
