13a86521b818ee5f0d9cf7e8d71a38443a0a1b21
SooYeon Seong <soolotus632@gmail.com>
2024-12-29 08:43:56 -0700

    KeyError handling

--- main.py
+++ main.py
@@ -8,23 +8,25 @@ days = st.slider("Forecast Days", min_value=1, max_value=5, help="Select the num
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 if place:
-    filtered_data = get_data(place, days)
+    try:
+        filtered_data = get_data(place, days)
 
+        if option == "Temperature":
+            temperature = [dict["main"]["temp"]/10 for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            figure = px.line(x=dates, y=temperature, labels={"x": "Date", "y":"Temperature(C)"})
+            st.plotly_chart(figure)
 
-    if option == "Temperature":
-        temperature = [dict["main"]["temp"] for dict in filtered_data]
-        dates = [dict["dt_txt"] for dict in filtered_data]
-        figure = px.line(x=dates, y=temperature, labels={"x": "Date", "y":"Temperature(C)"})
-        st.plotly_chart(figure)
-
-    if option == "Sky":
-        # filtered_data = [dict["weather"][0]["main"].lower() for dict in filtered_data]
-        # for image_file in filtered_data:
-        #     st.image(f"images/{image_file}.png")
-        sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
-        images = {"Clear": "images/clear.png",
-                  "Clouds": "images/clouds.png",
-                  "Rain":"images/rain.png",
-                  "Snow": "images/snow.png"}
-        image_paths = [images[condition] for condition in sky_conditions]
-        st.image(image_paths, width=115)
+        if option == "Sky":
+            # filtered_data = [dict["weather"][0]["main"].lower() for dict in filtered_data]
+            # for image_file in filtered_data:
+            #     st.image(f"images/{image_file}.png")
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            images = {"Clear": "images/clear.png",
+                      "Clouds": "images/clouds.png",
+                      "Rain":"images/rain.png",
+                      "Snow": "images/snow.png"}
+            image_paths = [images[condition] for condition in sky_conditions]
+            st.image(image_paths, width=115)
+    except KeyError:
+        st.write("That city does not exist")
